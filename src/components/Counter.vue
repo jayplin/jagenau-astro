f8fde644ad480aaeb24ac736ef6c5563820d9bcf
Jannik Schröer <jannik.schroeer@sap.com>
2022-12-15 15:45:54 +0100

    chore: replace naive with headless-ui

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,8 +1,9 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
 import { useNow } from '@vueuse/core'
-import pkg from 'naive-ui';
-const { NButton } = pkg;
+import { Switch } from '@headlessui/vue'
+
+const enabled = ref(false)
 
 
 const count = ref(0);
@@ -13,10 +14,24 @@ const subtract = () => (count.value = count.value - 1);
 	
 <template>
 	<div class="counter">
-		<n-button @click="subtract()">-</n-button>
+		<button @click="subtract()">-</button>
 		<pre>{{ count }}</pre>
 		<button @click="add()">+</button>
 	</div>
+	<div class="py-16">
+    <Switch
+      v-model="enabled"
+      :class="enabled ? 'bg-teal-900' : 'bg-teal-700'"
+      class="relative inline-flex h-[38px] w-[74px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+    >
+      <span class="sr-only">Use setting</span>
+      <span
+        aria-hidden="true"
+        :class="enabled ? 'translate-x-9' : 'translate-x-0'"
+        class="pointer-events-none inline-block h-[34px] w-[34px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out"
+      />
+    </Switch>
+		</div>
 	<div class="counter-message">
 		<slot />
 	</div>
